5d492fd5dd897e74cb24ae63104711126bd4c005
EmptyTeacup-afk <rbeaumont744@gmail.com>
2024-10-09 17:51:28 +0200

    Updated orders

--- foundation/Foundation2/Order.cs
+++ foundation/Foundation2/Order.cs
@@ -1,20 +1,20 @@
 class Order
 {
-    private Customer customer;
-    private List<Product> products;
+    private Customer _customer;
+    private List<Product> _products;
 
     public Order(Customer customer, List<Product> products)
     {
-        this.customer = customer;
-        this.products = products;
+        this._customer = customer;
+        this._products = products;
     }
 
-    public Customer Customer => customer;
-    public List<Product> Products => products;
-    public decimal GetTotalCost() => products.Sum(p => p.GetTotalCost());
-    public decimal GetShippingCost() => customer.IsInUSA() ? 5 : 35;
+    public Customer Customer => _customer;
+    public List<Product> Products => _products;
+    public decimal GetTotalCost() => _products.Sum(p => p.GetTotalCost());
+    public decimal GetShippingCost() => _customer.IsInUSA() ? 5 : 35;
     public decimal GetTotalPrice() => GetTotalCost() + GetShippingCost();
-    public string GetPackingLabel() => string.Join("\n", products.Select(p => $"{p.Name} ({p.ProductId})"));
-    public string GetShippingLabel() => $"{customer.Name}\n{customer.Address}";
+    public string GetPackingLabel() => string.Join("\n", _products.Select(p => $"{p.Name} ({p.ProductId})"));
+    public string GetShippingLabel() => $"{_customer.Name}\n{_customer.Address}";
     
 }
